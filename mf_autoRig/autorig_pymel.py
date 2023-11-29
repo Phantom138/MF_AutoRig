e9f0d53497f777638f6dc608878e523b9bed0d03
332770 <332770@via.dk>
2023-11-29 16:32:23 +0100

    added ikfk switch

--- mf_autoRig/autorig_pymel.py
+++ mf_autoRig/autorig_pymel.py
@@ -2,6 +2,8 @@ import pymel.core as pm
 import pymel.core.datatypes as dt
 import maya.cmds as cmds
 import maya.api.OpenMaya as om
+import re
+
 
 grp_sff = '_grp'
 ctrl_sff = '_ctrl'
@@ -49,7 +51,7 @@ CTRL_SHAPES = dict(
             [0, 1, 2, 3, 4, 5, 6, 7]]
 )
 
-CTRL_SCALE = 5
+CTRL_SCALE = 10
 
 for shape in CTRL_SHAPES:
     CTRL_SHAPES[shape][1] = [(x * CTRL_SCALE, y * CTRL_SCALE, z * CTRL_SCALE) for x, y, z in CTRL_SHAPES[shape][1]]
@@ -106,6 +108,7 @@ def fk_controllers(joints):
         axis = (1, 0, 0)
     # print(axis)
 
+    fk_ctrls = []
     ctrl_previous = None
     for jnt in joints[:-1]:
         # Skip end joints
@@ -118,7 +121,7 @@ def fk_controllers(joints):
 
         # Create controller and controller group, parenting the two of them
         grp = pm.createNode('transform', name=base_name + ctrl_sff + grp_sff)
-        ctrl = pm.circle(nr=axis, c=(0, 0, 0), name=base_name + ctrl_sff)
+        ctrl = pm.circle(nr=axis, c=(0, 0, 0), radius=CTRL_SCALE, name=base_name + ctrl_sff)
         pm.parent(ctrl, grp)
 
         # Match transforms and parent constrain controller to joint
@@ -129,7 +132,9 @@ def fk_controllers(joints):
         if ctrl_previous is not None:
             pm.parent(grp, ctrl_previous)
         ctrl_previous = ctrl
+        fk_ctrls.append(ctrl[0])
 
+    return fk_ctrls
 
 def create_pole_vector(joints):
     A = dt.Vector(pm.xform(joints[0], worldSpace=True, rotatePivot=True, q=True))
@@ -179,13 +184,15 @@ def create_ik_joints(joints):
     handle_name = first_jnt_name + '_ikHandle'
     ikHandle = pm.ikHandle(name=handle_name, startJoint=ik_joints[0], endEffector=ik_joints[2], solver='ikRPsolver')
 
-    # Create group and controller for ik
+    ik_ctrls = []
+    # Create group and controller for ikHandle
     grp = pm.createNode('transform', name=first_jnt_name + ik_sff + ctrl_sff + grp_sff)
     ctrl = pm.curve(degree=CTRL_SHAPES['cube'][0], point=CTRL_SHAPES['cube'][1], knot=CTRL_SHAPES['cube'][2], name=first_jnt_name + ik_sff + ctrl_sff)
     pm.parent(ctrl, grp)
     pm.matchTransform(grp, ikHandle, position=True)
 
     pm.parentConstraint(ctrl, ikHandle[0], maintainOffset=True)
+    ik_ctrls.append(ctrl)
 
     # Pole Vector
     pole_name = base_name + pole_sff + ctrl_sff
@@ -200,30 +207,94 @@ def create_ik_joints(joints):
     pm.move(pole_vector.x, pole_vector.y, pole_vector.z, pole_grp, worldSpace=True)
     pm.poleVectorConstraint(pole_ctrl, ikHandle[0])
 
-    return ik_joints
+    ik_ctrls.append(pole_ctrl)
+
+    return ik_joints, ik_ctrls
 
 
-def constraint_fkik(skin_joints, ik_joints, fk_joints):
+def ikfk_constraint(skin_joints, ik_joints, fk_joints):
     fkik_constraints = []
     if len(skin_joints) is not len(fk_joints) is not len(ik_joints) is not 3:
         pm.error("Ik FK Joints not matching")
+
 
     for i in range(len(skin_joints)):
         constraint = pm.parentConstraint(ik_joints[i], fk_joints[i], skin_joints[i])
         fkik_constraints.append(constraint)
         # print(pm.listConnections(constraint, destination=True, source=True))
+
+    #grp = pm.createNode('transform', name='test_grp')
+
+
     return fkik_constraints
 
+def ikfk_switch(ik_ctrls, fk_ctrls, ikfk_constraints, endJnt):
+    '''
+    Method:
+    ik Fk Switch = fk weight
+    ik Fk Switch * (-1) = ik weight
+    '''
+    # Get base name of the joint (L_arm01_skin_jnt -> L_arm
+    match = re.search('([a-zA-Z]_[a-zA-Z]+)\d*_', endJnt.name())
+    base_name = match.group(1)
+
+    name = base_name + '_ikfkSwitch' + ctrl_sff
+
+    # Create switch controller and grp
+    switch_grp = pm.createNode('transform', name=name + '_grp')
+    switch_ctrl = pm.curve(degree=CTRL_SHAPES['arrow'][0], point=CTRL_SHAPES['arrow'][1], knot=CTRL_SHAPES['arrow'][2], name=name)
+    pm.parent(switch_ctrl, switch_grp)
+    print(endJnt)
+    pm.matchTransform(switch_grp, endJnt)
+    pm.parentConstraint(switch_ctrl, endJnt, maintainOffset=True)
+
+    # Add ikfk switch attribute
+    pm.addAttr(switch_ctrl, longName="IkFkSwitch", attributeType='float', min=0, max=1, defaultValue=1, keyable=True)
+
+    # Reverse node
+    reverse_sw = pm.createNode('reverse', name=base_name + '_Ik_Fk_reverse')
+    pm.connectAttr(switch_ctrl + '.IkFkSwitch', reverse_sw + '.inputX')
+
+    # For each constraint get the weight names and connect them accrodingly
+    for constraint in ikfk_constraints:
+        weights = constraint.getWeightAliasList()
+        for weight in weights:
+            name = weight.longName(fullPath=False)
+            # If ik weight connect to reverse
+            if ik_sff in name:
+                pm.connectAttr(reverse_sw + '.outputX', weight)
+            # If fk weight connect to switch
+            if fk_sff in name:
+                pm.connectAttr(switch_ctrl + '.IkFkSwitch', weight)
+
+    # Hide ik or fk ctrls based on switch
+    for ctrl in ik_ctrls:
+        pm.connectAttr(reverse_sw + '.outputX', ctrl+'.visibility')
+
+    for ctrl in fk_ctrls:
+        print(ctrl)
+        print(fk_ctrls)
+        pm.connectAttr(switch_ctrl + '.IkFkSwitch', ctrl+'.visibility')
 
 def create_fkik(joints):
     fk_joints = create_fk_joints(joints)
     print(fk_joints)
-    fk_controllers(fk_joints)
-    ik_joints = create_ik_joints(joints)
-    #fkik_constraints = constraint_fkik(joints, ik_joints, fk_joints)
-    # fkik_switch(joints, fkik_constraints)
+    fk_ctrls = fk_controllers(fk_joints)
+    print(fk_ctrls)
+    ik_joints, ik_ctrls = create_ik_joints(joints)
+    fkik_constraints = ikfk_constraint(joints, ik_joints, fk_joints)
+    ikfk_switch(ik_ctrls, fk_ctrls, fkik_constraints, joints[-1])
+
+def make_ctrl_bigger(selection):
+    for sl in selection:
+        if sl.getShape().type() == 'nurbsCurve':
+            cvs = sl.getCVs()
+            cvs = [cv * 1.25 for cv in cvs]
+            sl.setCVs(cvs)
+            sl.updateCurve()
 
 
 selection = pm.selected()
+
 create_fkik(selection)
 
