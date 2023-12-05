922fff6ad9cb8c5b4b37004cbeacffa1bf1ec83e
332770 <332770@via.dk>
2023-12-05 16:09:39 +0100

    connect hand

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ attr_sff = '_attr'
 
 CTRL_SCALE = 2
 
-def connect_hand(handJnt, arm_end_jnt, arm_ik_ctrl, arm_fk_ctrl):
+def connect_hand(handJnt, arm_end_jnt, arm_ik_ctrl, arm_fk_ctrl, ikfkController):
     match = re.search('([a-zA-Z]_([a-zA-Z]+))\d*_', handJnt.name())
     base_name = match.group(1)
 
@@ -38,10 +38,6 @@ def connect_hand(handJnt, arm_end_jnt, arm_ik_ctrl, arm_fk_ctrl):
     pm.matchTransform(ik_loc_grp, handJnt)
     pm.parent(ik_loc_grp, arm_ik_ctrl)
 
-    constraint = pm.orientConstraint(ik_loc, hand_ctrl)
-    print(pm.listAttr(constraint))
-    print(constraint.listAliases())
-
     # FK locator
     fk_loc = pm.spaceLocator(name=base_name+'_fk_space_loc')
     fk_loc_grp = pm.createNode('transform', name=base_name + '_ik_loc_grp')
@@ -49,10 +45,29 @@ def connect_hand(handJnt, arm_end_jnt, arm_ik_ctrl, arm_fk_ctrl):
     pm.matchTransform(fk_loc_grp, handJnt)
     pm.parent(fk_loc_grp, arm_fk_ctrl)
 
+    constraint = pm.orientConstraint(ik_loc, fk_loc, hand_ctrl)
+    weights = constraint.getWeightAliasList()
+    print(weights)
+
+    for weight in weights:
+        ikfkSwitch = pm.Attribute(ikfkController + '.IkFkSwitch')
+        # Get reverse node and switch
+        reverseNode = pm.listConnections(ikfkSwitch, destination=True, type='reverse')[0]
+
+
+        # Connect Weights accordingly
+        if fk_sff in weight.name():
+            ikfkSwitch.connect(weight)
+        elif ik_sff in weight.name():
+            reverseNode.outputX.connect(weight)
+
 
 handJnt = pm.PyNode('R_hand_skin_jnt')
 arm_ik_ctrl = pm.PyNode('R_arm01_ik_ctrl')
 arm_fk_ctrl = pm.PyNode('R_arm02_fk_ctrl')
 arm_end_jnt = pm.PyNode('R_arm03_end_jnt')
+ikfkController = pm.PyNode('R_arm_ikfkSwitch_ctrl')
 
-connect_hand(handJnt, arm_end_jnt, arm_ik_ctrl, arm_fk_ctrl)
+print(ikfkController.IkFkSwitch)
+
+#connect_hand(handJnt, arm_end_jnt, arm_ik_ctrl, arm_fk_ctrl, ikfkSwitch)
