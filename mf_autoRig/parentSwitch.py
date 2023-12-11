3b299a1676574e37f6cd3fcf1000e2c977d2bf2a
Mihai <mihai.filat@yahoo.com>
2023-12-12 00:17:17 +0100

    small changes

--- mf_autoRig/parentSwitch.py
+++ mf_autoRig/parentSwitch.py
@@ -1,21 +1,67 @@
+import re
+
 import pymel.core as pm
 
-def parent_switch(object, drivers, jnt):
+
+def parent_switch(object, drivers, jnt, hasGroup=True):
+    # Get nice name for object
+    object_match = re.search(r'^[a-zA-Z]_*[a-zA-Z]+\d*', object.name())
+    object_name = object_match.group(0)
+
     # Create Locators
     locators = []
+    enum_names = ''
+
+    if hasGroup:
+        grp = object.getParent(1)
+    else:
+        grp = object
+
     for driver in drivers:
-        loc = pm.spaceLocator(name=f'{object.name()}_{driver.name()}_space_loc')
+        # Get nice name for drivers, to be used in enum
+        # If it has a prefix, take only what is after
+        if re.search(r'^[a-zA-Z]_', driver.name()):
+            print(f'{driver.name()} starts with prefix')
+            driver_match = re.search(r'^[a-zA-Z]_([a-zA-Z]+\d*)', driver.name())
+            driver_name = driver_match.group(1)
+        else:
+            driver_match = re.search('^([a-zA-Z]+)_', driver.name())
+            driver_name = driver_match.group(1)
+
+        enum_names += driver_name+':'
+
+        # Create locator and parent it in the right place
+        loc = pm.spaceLocator(name=f'{object_name}_{driver_name}_space_loc')
         pm.matchTransform(loc, jnt)
-
+        pm.parent(loc, driver)
         locators.append(loc)
-    for locator in locators:
-        pm.parent()
-    pm.addAttr(object, ln='parentSwitch', type='enum', k=True)
 
-    #for locator in locators:
+    # Create enum attributs, enum_names = Root:World:...
+    pm.addAttr(object, ln='parentSwitch', attributeType='enum', en=enum_names, k=True)
+
+    # Create parent constriant
     print(locators)
+    constraint = pm.parentConstraint(locators, grp, maintainOffset=True)
+    weights = constraint.getWeightAliasList()
 
-obj = pm.PyNode('M_eye_aim_ctrl')
+    for index, weight in enumerate(weights):
+        # Get weight name for the condition
+        weight_match = re.search(r'([a-zA-Z]+_[a-zA-Z]+)_space_loc', weight.name())
+        weight_name = weight_match.group(1)
+
+        # Create condition and connect it accordingly
+        condition = pm.createNode('condition', name=weight_name+'_condition')
+
+        object.parentSwitch.connect(condition.firstTerm)
+
+        condition.secondTerm.set(index)
+        condition.colorIfTrueR.set(1)
+        condition.colorIfFalseR.set(0)
+        condition.outColorR.connect(weight)
+
+
+obj = pm.PyNode('M_eyeAim_ctrl')
 jnt = pm.PyNode('M_head_skin_jnt')
-drivers = [pm.PyNode("M_head_skin_jnt"), pm.PyNode("Root_Ctrl")]
+drivers = [pm.PyNode("M_head_ctrl"), pm.PyNode("Root_Ctrl"), pm.PyNode("World_Ctrl")]
+
 parent_switch(obj, drivers, jnt)
