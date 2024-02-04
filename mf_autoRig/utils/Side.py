3fc97f471eb68a401443a9d0c3fbbe0ab10b8098
Mihai Filat <mihai.filat@yahoo.com>
2024-02-04 19:18:44 +0200

    Added Side class, implemented almost everywere

---
+++ mf_autoRig/utils/Side.py
@@ -0,0 +1,19 @@
+class Side:
+    def __init__(self, side: str):
+        self.side = side
+
+        left = ['L', 'l', 'Left', 'left']
+        right = ['R', 'r', 'Right', 'right']
+        mid = ['M', 'm', 'Middle', 'middle']
+
+        if side in left:
+            self.opposite = right[left.index(side)]
+        elif side in right:
+            self.opposite = left[right.index(side)]
+        elif side in mid:
+            pass
+        else:
+            raise ValueError(f'Side not named properly -> {side}')
+
+    def __str__(self):
+        return self.side
