7daf0fb7f512d0bc4f3fda9672f565e68670edc9
Phantom138 <mihai.filat@yahoo.com>
2025-03-20 17:33:33 +0100

    fixed ikfk switch

--- mf_autoRig/extras/ikfk_switch.py
+++ mf_autoRig/extras/ikfk_switch.py
@@ -120,50 +120,38 @@ def switch_ik_fk(switch_ctrl):
 class IkFkSwitch:
     def __init__(self, switch_ctrl):
         self.switch_ctrl = switch_ctrl
-        self.ik_fk_attr = self.switch_ctrl.IkFkSwitch
+        self.ik_fk_attr = switch_ctrl.IkFkSwitch
 
-        meta_lst = switch_ctrl.message.listConnections(t="network")
-        if meta_lst:
-            self.meta_node = meta_lst[0]
+        self.joints = switch_ctrl.joints.get()
+
+        self.ik_joints = switch_ctrl.ik_joints.get()
+        self.fk_joints = switch_ctrl.fk_joints.get()
+
+        self.fk_ctrls = switch_ctrl.fk_ctrls.get()
+        self.ik_ctrls = switch_ctrl.ik_ctrls.get()
+
+        self.has_foot = switch_ctrl.has_foot.get()
+        if self.has_foot:
+            self.foot_joints = switch_ctrl.foot_joints.get()
+            self.foot_fk_ctrls = switch_ctrl.foot_fk_ctrls.get()
+            self.foot_offset = switch_ctrl.foot_offset.get()
         else:
-            self.meta_node = None
-            raise ValueError(f'{switch_ctrl} is not part of any module')
-
-        if self.meta_node.moduleType.get() != 'Limb':
-            raise ValueError(f'{self.meta_node} of type {self.meta_node.moduleType.get()}is not a Limb')
-
-        # Get switch_ctrl and see ik/fk value
-
-        self.joints = self.meta_node.joints.get()
-
-        self.ik_joints = self.meta_node.ik_joints.get()
-        self.fk_joints = self.meta_node.fk_joints.get()
-
-        self.fk_ctrls = self.meta_node.fk_ctrls.get()
-        self.ik_ctrls = self.meta_node.ik_ctrls.get()
-
-        # Connected to foot?
-        node = [a for a in self.meta_node.children.get() if a.moduleType.get() == 'IKFoot' ]
-        if node:
-            self.foot = node[0]
-        else:
-            self.foot = None
+            self.foot_joints = []
+            self.fk_ctrls = []
 
     def ik_to_fk(self):
         for jnt, fk in zip(self.ik_joints[:2], self.fk_ctrls[:2]):
             mtx = jnt.worldMatrix.get() * fk.parentInverseMatrix.get()
             pm.xform(fk, m=mtx)
-            # Keyframes
-            # fk.r.setKey()
-
+            
         if len(self.fk_ctrls) == 3:
             # Match end with ik_ctrl
             mtx = self.ik_ctrls[0].worldMatrix.get() * self.fk_ctrls[-1].parentInverseMatrix.get()
             pm.xform(self.fk_ctrls[-1], m=mtx)
 
         # If connected to foot
-        if self.foot is not None:
-            for foot_jnt, fk_ctrl in zip(self.foot.joints.get(), self.foot.fk_ctrls.get()):
+        if self.has_foot:
+            for foot_jnt, fk_ctrl in zip(self.foot_joints, self.foot_fk_ctrls):
                 mtx = foot_jnt.worldMatrix.get() * fk_ctrl.parentInverseMatrix.get()
                 pm.xform(fk_ctrl, m=mtx)
 
@@ -173,37 +161,43 @@ class IkFkSwitch:
         pole_ctrl = self.ik_ctrls[1]
 
         # Set IK
-        if self.foot is not None:
-            # Match rotation of fk ankle to ik ctrl
-            # This more complex setup is needed since they are oriented in different ways
-            ankle = self.foot.fk_ctrls.get()[0]
-            fk_parent_inv_mtx = ankle.parentInverseMatrix.get()
-            ik_ctrl_parent_mtx = ik_ctrl.parentMatrix.get()
-            fk_world_mtx = ankle.worldMatrix.get()
+        if self.has_foot:
+            tmp_loc = pm.spaceLocator()
+            tmp_loc.r.set(self.foot_offset)
+            
+            res_mtx = tmp_loc.worldMatrix.get() * self.foot_fk_ctrls[0].worldMatrix.get()
+            
+            pm.xform(tmp_loc, m=res_mtx) # This is only for rotation
+            ik_ctrl.r.set(tmp_loc.r.get())
+            pm.delete(tmp_loc)
 
-            res_mtx = fk_parent_inv_mtx * ik_ctrl_parent_mtx * fk_world_mtx
-            pm.xform(ik_ctrl, m=res_mtx) # This is only for rotation
-            pm.matchTransform(ik_ctrl, ankle, position=True)
-
+            pm.matchTransform(ik_ctrl, self.foot_fk_ctrls[0], position=True)
+            
         else:
             ik_mtx = self.fk_joints[-1].worldMatrix.get() * ik_ctrl.parentInverseMatrix.get()
             pm.xform(ik_ctrl, m=ik_mtx)
 
         # Set pole
-        pole_parent_t = pole_ctrl.getParent(1).t.get()
-
         target_t = create_pole_vector(self.fk_joints)
-        res_t = target_t - pole_parent_t
-        pole_ctrl.t.set(res_t)
+        tmp_loc = pm.spaceLocator()
+        tmp_loc.t.set(target_t)
+
+        pm.matchTransform(pole_ctrl, tmp_loc)
+        pm.delete(tmp_loc)
 
 
-    def switch(self):
+    def switch(self, key=False):
         if self.ik_fk_attr.get() == 0:
             # In ik mode
             self.ik_to_fk()
+            if key:
+                self.fk_setKey()
         else:
             # in fk mode
             self.fk_to_ik()
+            if key:
+                self.ik_setKey()
+    
     def ik_setKey(self):
         self.ik_ctrls[0].t.setKey()
         self.ik_ctrls[0].r.setKey()
@@ -214,6 +208,121 @@ class IkFkSwitch:
             fk.r.setKey()
 
         if self.foot is not None:
+            for ctrl in self.foot.fk_ctrls.get():
+                ctrl.r.setKey()
+    
+    def bake_framerange(self):
+        # Check that the switch value doesn't change in the fram range
+        start = int(pm.playbackOptions(q=True, min=True))
+        end = int(pm.playbackOptions(q=True, max=True))
+
+        start_switch_value = self.ik_fk_attr.get(time=start)
+        for i in range(start, end+1):
+            val = self.ik_fk_attr.get(time=i)
+            if val != start_switch_value:
+                pm.error(f"Frame range has different switch values at frame {i}")
+        
+        for i in range(start, end+1):
+            pm.currentTime(i, e=True)
+            self.switch(key=True)
+
+# -------- Functions for creating switch ---------- #
+def create_switch(switch_ctrl):
+    meta_lst = switch_ctrl.message.listConnections(t="network")
+    if meta_lst:
+        meta_node = meta_lst[0]
+    else:
+        meta_node = None
+        raise ValueError(f'{switch_ctrl} is not part of any module')
+
+    if meta_node.moduleType.get() != 'Limb':
+        raise ValueError(f'{meta_node} of type {meta_node.moduleType.get()}is not a Limb')
+
+    # Get switch_ctrl and see ik/fk value
+    data = {}
+    data["joints"] = meta_node.joints.get()
+
+    data["ik_joints"] = meta_node.ik_joints.get()
+    data["fk_joints"] = meta_node.fk_joints.get()
+
+    data["fk_ctrls"] = meta_node.fk_ctrls.get()
+    data["ik_ctrls"] = meta_node.ik_ctrls.get()
+
+    # Connected to foot?
+    node = [a for a in meta_node.children.get() if a.moduleType.get() == 'IKFoot' ]
+
+    data["has_foot"] = False
+    data["foot_joints"] = []
+    data["foot_fk_ctrls"] = []
+    data["foot_offset"] = []
+
+    if node:
+        data["has_foot"] = True
+        data["foot_fk_ctrls"] = node[0].fk_ctrls.get()
+        data["foot_joints"] = node[0].joints.get()
+        # calculate offset
+        fk_parent_inv_mtx = data["foot_fk_ctrls"][0].parentInverseMatrix.get()
+        ik_ctrl_parent_mtx = data["ik_ctrls"][0].parentMatrix.get()
+
+        offset_mtx = fk_parent_inv_mtx * ik_ctrl_parent_mtx 
+        tmp = pm.spaceLocator("temp_loc")
+        pm.xform(tmp, m=offset_mtx)
+        data["foot_offset"] = tmp.r.get()
+        pm.delete(tmp)
+        print(data["foot_offset"])
+    _clear_node(switch_ctrl, data)
+    _save_dict_to_node(switch_ctrl, data)
+
+def _save_dict_to_node(node, data: dict):
+    for key, item in data.items():
+        if isinstance(item, list):
+            if isinstance(item[0], pm.PyNode):
+                node.addAttr(key, attributeType='message', m=True)
+
+        elif isinstance(item, bool):
+            node.addAttr(key, attributeType='bool')
+        elif isinstance(item, pm.dt.Vector):
+            node.addAttr(key, attributeType='double3')
+            for axis in ['X', 'Y', 'Z']:
+                node.addAttr(key + axis, attributeType= 'double', p=key)
+        else:
+            pm.error(f"Cannot writ {item}")
+        _add(item, node.attr(key))
+
+def _clear_node(node, data:dict):
+    for key in data:
+        try:
+            node.deleteAttr(key)
+        except:
+            pass
+
+def _add(nodes, dst):
+    if not nodes:
+        return
+
+    def connect(src, dest):
+        # If it's a pynode connect message attr
+        if isinstance(src, pm.PyNode):
+            if not pm.isConnected(src.message, dest):
+                src.message.connect(dest)
+
+        # If it's an int just set the value
+        elif isinstance(src, (float, int, str)):
+            dest.set(src)
+
+        elif isinstance(src, bool):
+            dest.set(src)
+
+        # Set vector type variables
+        elif isinstance(src, pm.dt.Vector):
+            dest.set(src)
+
+    # If it's a list of nodes connect each one to the destination
+    if isinstance(nodes, list):
+        for i, node in enumerate(nodes):
+            connect(node, dst[i])
+    else:
+        connect(nodes, dst)
 
 
 def main():
@@ -225,3 +334,6 @@ def main():
     with UndoStack(f"Switching {node} IKFK"):
         sw = IkFkSwitch(node)
         sw.switch()
+
+main()
+# create_for_switch(pm.selected()[0])
